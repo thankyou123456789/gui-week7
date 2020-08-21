f83b54ef884810588abcb65ed7a8f8fb10775323
thankyou123456789 <69336071+thankyou123456789@users.noreply.github.com>
2020-08-21 08:28:15 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+function App(){
+return(
+  <View style={{
+    flexDirection:"row",
+    height:100,
+    padding:20
+  }}>
+  <View style={{backgroundColor:"blue",flex:0.3}} />
+  <View style={{backgroundColor:"red",flex:0.5}} />
+  <Text>I love cat</Text>
+  </View>
+  )
+}
+export default App;
